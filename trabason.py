7e55b145407352e6a941e633550c54a0f25a67b2
PedroBarrak <143905710+PedroBarrak@users.noreply.github.com>
2024-11-13 01:01:18 +0000

    FALTA FAZER 5 E 6

---
+++ trabason.py
@@ -0,0 +1,65 @@
+from validate_docbr import CPF
+import re
+
+#O programa coleta os dados (nome, CPF, email, etc.).
+
+nome = input("Digite seu nome: ")
+cpf = input("Digite seu cpf: ")
+
+#VALIDAÇÃO DE CPF
+cpf_validacao = CPF ()
+
+print("CPF VALIDADO!", cpf_validacao.validate(cpf) )
+
+
+#VALIDAÇÃO DE EMAIL
+
+def email_validacao(email):
+    validado = re.fullmatch(r'^[a-zA-Z0-9._-]+@([a-z0-9]+)(\.[a-z]{2,3})+$', email)
+    if validado:
+        return True
+    return False
+
+email = input("Digite seu e-mail: ")
+
+if email_validacao(email):
+    print("O e-mail é válido: VALIDO")
+else:
+    print("O e-mail é inválido: INVALIDO")
+
+#VALIDAÇÃO TELEFONE
+
+def telefone_validacao(telefone):
+    validado = re.fullmatch(r"\(\d{2}\) \d{5}-\d{4}", telefone)
+    if validado:
+        return True
+    return False
+
+telefone = input("Digite seu Telefone: ")
+
+print("TELEFONE: ", telefone_validacao(telefone))
+
+
+def site_por_matricula(matricula):
+    
+    lista = [
+        "Music Center",    # 0
+        "Amazon",          # 1
+        "Mercado Livre",   # 2
+        "Magazine Luiza",  # 3
+        "Casas Bahia",     # 4
+        "Mundomax",        # 5
+        "Carrefour",       # 6
+        "Americanas",      # 7
+        "Ponto Frio",      # 8
+        "Extra"            # 9
+    ]
+    
+    
+    ultimo_digito = int(str(matricula)[-1])
+    site = lista[ultimo_digito]
+    return site
+
+matricula = input("Digite o número da matrícula: ")
+site = site_por_matricula(matricula)
+print("O site correspondente ao último dígito da matrícula é: ", site) 
